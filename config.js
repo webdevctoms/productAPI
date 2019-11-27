a3e038f35956bd8c148b4b82aa290785ad37c484
Michael Peterson <WebDev@ctoms.ca>
2019-11-27 09:16:22 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,3 @@
+exports.PORT = process.env.PORT || 8000;
+exports.DATABASE_URL =  process.env.DATABASE_URL;
+exports.KEY =  process.env.KEY;
---
+++ server.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const {PORT, DATABASE_URL} = require('./config');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization,Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(204);
+    }
+    next();
+  });
+
+let server;
+
+function runServer( databaseUrl, port = PORT) {
+  return new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl,{ useNewUrlParser: true },err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+}
+
+function closeServer(){
+	return new Promise((resolve,reject) => {
+		console.log("closing server");
+		server.close(err => {
+			if(err){
+				return reject(err);
+			}
+			resolve();
+		});
+	});
+}
+
+if (require.main === module){
+	runServer(DATABASE_URL).catch(err => console.error(err));
+}
+
+module.exports = { app, runServer, closeServer };
